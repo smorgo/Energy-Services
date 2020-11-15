aa9378fb9e503bba666630de7426869133cb3c8b
Steve Morgan <steve@wormpurple.com>
2020-11-15 23:04:25 +0000

    Tweak to trigger Docker build

--- ImmersionController/Program.cs
+++ ImmersionController/Program.cs
@@ -7,7 +7,7 @@ namespace ImmersionController
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Immersion Controller starting");
+            Console.WriteLine("Immersion Controller starting now");
 
             new RateMonitor().Run();
 
